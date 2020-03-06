eb2ddc82d49519ccaf98779cd14165e48074998c
Greevee <Greevee@users.noreply.github.com>
2020-03-06 18:09:16 +0100

    Update modinfo.lua
    Version 1.3

--- modinfo.lua
+++ modinfo.lua
@@ -20,7 +20,7 @@ meta =
 
     -- Version of your mod, should be in format 1.0.0 (major.minor.patch) or 1.0 (major.minor)
     -- This will be used to check for unmet dependencies or incompatibilities
-    version = "1.2.7",
+    version = "1.3.0",
 
     -- If your mod requires dependencies, enter them here. The game will check that all dependencies given here are met.
     -- Possible attributes:
